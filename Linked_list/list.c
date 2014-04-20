0089903e6bd6d739a466d42de1a3fea0ca2f48d7
SalamanderMinds <shanjian1984@gmail.com>
2014-04-21 02:50:41 +0800

    update

--- Linked_list/list.c
+++ Linked_list/list.c
@@ -8,7 +8,10 @@ int revCom( p_list H ) {
 
 	char c, t;
 	
-	printf("ENTER:\n>b >c >d >e\n");
+	print(H);
+
+	printf("ENTER:\n>b: DELETE EVEN POSITIONS\n>c: FIND 100\n\
+>d: PRINT ODD POSITIONS\n>e: REVERSE LIST\n");
 
 	while ((t = getchar())) {
 		if ( t != '\n' ) break;
@@ -23,7 +26,7 @@ int revCom( p_list H ) {
 		//	DELETE EVEN POSITIONS ELEMENTS
 		//	FIRST REALL NODE IS THE 0TH POSITION
 		printf("\nDELETE EVEN POSITIONS.\n");
-		double_del(H); print(H); return 1;
+		double_del(H); return 1;
 	}
 	else if (c == 'c') {
 		//	FIND IF 100 IN THE LIST
@@ -40,7 +43,7 @@ int revCom( p_list H ) {
 	else if (c == 'e') {
 		//	REVERSE THE LIST
 		printf("\nREVERSE THE LIST.\n");
-		reverse(H); print(H); return 1;
+		reverse(H); return 1;
 	}
 	else {
 		//	COMMAND NOT FOUND
@@ -62,8 +65,10 @@ FILE * fp;
 p_list read( p_list H) {
 
 	fp = fopen("data100.txt", "r");
-	if (!fp)
-		printf("FILE IS NOT FOUND.\n");
+	if (!fp) {
+		printf("FILE IS NOT FOUND.\n\n");
+		return NULL;
+	}
 	
 	H = new(H);		// CREATE HEADER
 	p_list L = H;	// START FROM GIVEN HEADER
@@ -112,14 +117,16 @@ void print( p_list H ) {
 	p_list p = H->next;
 
 	if (p == NULL) {
-		printf("ERROR: NULL LINKED LIST!\n");
+		printf("ERROR: NULL LINKED LIST!\n\n");
 		return;
 	}
 
+	printf("The table is:\n");
+
 	while ( p->next ) {
-		printf( "%10d", p->x );
+		printf( "%6d", p->x );
 		p = p->next;
-	} printf( "%10d\n", p->x );
+	} printf( "%6d\n\n", p->x );
 }
 
 // REVERSE A LIST
@@ -128,7 +135,7 @@ void reverse(p_list H) {
 	p_list p = S;
 
 	if (S == NULL) {
-		printf("ERROR: NULL LINKED LIST!\n");
+		printf("ERROR: NULL LINKED LIST!\n\n");
 		return;
 	} else {
 		while (1) {
@@ -137,38 +144,39 @@ void reverse(p_list H) {
 			while (p->next)
 				p = p->next;	// FIND THE LAST NODE
 
-			p->prev->next = NULL;	// SWAP
-			p->next = S; p->prev = H;
-			H->next = p;
-			S->prev = p; S = p;
+			p->prev->next = NULL;
+			p->prev = S->prev; S->prev->next = p;
+			p->next = S; S->prev = p;
+			p = S;
 		}
 	} return;
 }
 
 // PRINT ODD POSITIONS
 void odd_print(p_list H) {
-	p_list p = H->next;
+
+	p_list p = H->next;	// START FROM THE NODE 0
 
 	if (p == NULL) {
-		printf("ERROR: NULL LINKED LIST!\n");
+		printf("ERROR: NULL LINKED LIST!\n\n");
 		return;
 	}
-	if (p->next == NULL) {
-		printf("ERROR: ONE NODE ONLY!\n");
+	if (p->next == NULL) {	// THE NODE 1
+		printf("ERROR: ONE NODE ONLY!\n\n");
 		return;
 	} p = p->next;
 
 	while (1) {
 
-		if (p->next == NULL || p == NULL)
+		if (p == NULL)
 			break;
-		else if (p->next == NULL) {
-			printf("%10d", p->x);
-			p = p->next->next;
-		} else {
-			printf("%10d\n", p->x);
+		else if (p->next == NULL) {	// EVEN (NEXT) POSITION IS NULL
+			printf("%6d\n", p->x);	// END
 			p = NULL;
-		}
+		} else {
+			printf("%6d", p->x);
+			p = p->next->next;	// JUMP TO ODD POSITION
+		} printf("\n");
 	} return;
 }
 
@@ -178,27 +186,22 @@ void double_del(p_list H) {
 	p_list t;
 
 	if (p == NULL) {
-		printf("ERROR: NULL LINKED LIST!\n");
+		printf("ERROR: NULL LINKED LIST!\n\n");
 		return;
 	}
 
 	while (1) {
 
-		if (p->next == NULL || p == NULL)
+		if (p == NULL)
 			break;
-		else {
-
+		else if (p->next == NULL) {	// NEXT POSITION IS NULL
+			p->prev->next = NULL;
+			free(p); p = NULL;	// END
+		} else {
 			p->next->prev = p->prev;
-
-			if (p->next->next) {
-				p->prev->next = p->next;
-				t = p->next->next;
-				free(p); p = t;
-			}
-			else {
-				p->prev->next = NULL;
-				free(p); p = NULL;
-			}
+			p->prev->next = p->next;
+			t = p->next->next;	// JUMP
+			free(p); p = t;
 		}
 	} return;
 }
